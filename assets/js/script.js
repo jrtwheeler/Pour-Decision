e629cdd97e7c3f90d1943631bf6901dfcc88770f
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-10-01 21:15:59 -0400

    Show food and dirnk div onclick

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,107 +1,109 @@
+//Hide food and drink divs
 var food_div = $(".object-food").hide();
 var food_div = $(".object-drink").hide();
 
 $("#getRecipes").on("click", function () {
-    var foodChoice = $("#foodChoice").val();
-    var drinkChoice = $("#drinkChoice").val();
+  //Show food and drink div
+  var food_div = $(".object-food").show();
+  var food_div = $(".object-drink").show();
+  var foodChoice = $("#foodChoice").val();
+  var drinkChoice = $("#drinkChoice").val();
 
-    var foodURL = "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + foodChoice;
-    var drinkURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=" + drinkChoice;
+  var foodURL =
+    "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + foodChoice;
+  var drinkURL =
+    "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=" + drinkChoice;
 
+  $.ajax({
+    url: foodURL,
+    method: "GET",
+  }).then(function (response) {
+    var foodCount = response.meals.length;
+    var foodPick = Math.floor(Math.random() * foodCount);
+    var foodRand = response.meals[foodPick].idMeal;
 
-    $.ajax({
-        url: foodURL,
-        method: "GET"
-    }) .then(function (response) {
-        var foodCount = response.meals.length;
-        var foodPick = Math.floor(Math.random()*foodCount);
-        var foodRand = response.meals[foodPick].idMeal;
-
-        var newFoodURL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + foodRand;
-
-        $.ajax({
-            url: newFoodURL,
-            method: "GET"
-        }) .then(function (response) {
-            dispFood(response);
-        });
-
-    });
+    var newFoodURL =
+      "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + foodRand;
 
     $.ajax({
-        url: drinkURL,
-        method: "GET"
-    }) .then(function (response) {
-        var drinkCount = response.drinks.length;
-        var drinkPick = Math.floor(Math.random()*drinkCount);
-        var drinkRand = response.drinks[drinkPick].idDrink;
-
-        var newDrinkURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkRand;
-
-        $.ajax({
-            url: newDrinkURL,
-            method: "GET"
-        }) .then(function (response) {
-            dispDrink(response);
-        });
-
+      url: newFoodURL,
+      method: "GET",
+    }).then(function (response) {
+      dispFood(response);
     });
+  });
 
-})
+  $.ajax({
+    url: drinkURL,
+    method: "GET",
+  }).then(function (response) {
+    var drinkCount = response.drinks.length;
+    var drinkPick = Math.floor(Math.random() * drinkCount);
+    var drinkRand = response.drinks[drinkPick].idDrink;
 
-function dispFood (res) {
-    console.log(res);
-    var meal = res.meals[0];
-    var i = 1;
-    var stop = false;
-    console.log(meal.strMeal);
-    $("#foodMeal").text(meal.strMeal);
-    $("#foodCategory").text(meal.strCategory);
-    $("#foodOrigin").text(meal.strArea);
-    $("#foodInstructions").text(meal.strInstructions);
-    $("#foodImage").attr("src", meal.strMealThumb);
-    $("#foodVideo").attr("src", meal.strYoutube);
-    while (!stop) {
-        if (meal['strIngredient' + i]) {
-            var trDiv = $("<tr>");
-            var tdDiv1 = $("<td>");
-            var tdDiv2 = $("<td>");
-            tdDiv1.text(meal['strIngredient' + i]);
-            tdDiv2.text(meal['strMeasure' + i]);
-            trDiv.append(tdDiv1, tdDiv2);
-            $("#foodIngredient").append(trDiv);
-            i++;
-        } else {
-            stop = true;
-        };
-    };  
+    var newDrinkURL =
+      "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkRand;
 
+    $.ajax({
+      url: newDrinkURL,
+      method: "GET",
+    }).then(function (response) {
+      dispDrink(response);
+    });
+  });
+});
+
+function dispFood(res) {
+  console.log(res);
+  var meal = res.meals[0];
+  var i = 1;
+  var stop = false;
+  console.log(meal.strMeal);
+  $("#foodMeal").text(meal.strMeal);
+  $("#foodCategory").text(meal.strCategory);
+  $("#foodOrigin").text(meal.strArea);
+  $("#foodInstructions").text(meal.strInstructions);
+  $("#foodImage").attr("src", meal.strMealThumb);
+  $("#foodVideo").attr("src", meal.strYoutube);
+  while (!stop) {
+    if (meal["strIngredient" + i]) {
+      var trDiv = $("<tr>");
+      var tdDiv1 = $("<td>");
+      var tdDiv2 = $("<td>");
+      tdDiv1.text(meal["strIngredient" + i]);
+      tdDiv2.text(meal["strMeasure" + i]);
+      trDiv.append(tdDiv1, tdDiv2);
+      $("#foodIngredient").append(trDiv);
+      i++;
+    } else {
+      stop = true;
+    }
+  }
 }
 
-function dispDrink (res) {
-    console.log(res);
-    var drink = res.drinks[0]
-    var i = 1;
-    var stop = false;
-    $("#drinkName").text(drink.strDrink);
-    $("#drinkCategory").text(drink.strCategory);
-    $("#drinkAlcoholic").text(drink.strAlcoholic);
-    $("#drinkGlass").text(drink.strGlass);
-    $("#drinkInstructions").text(drink.strInstructions);
-    $("#drinkImage").attr("src", drink.strDrinkThumb);
-    while (!stop) {
-        if (drink['strIngredient' + i]) {
-            var trDiv = $("<tr>");
-            var tdDiv1 = $("<td>");
-            var tdDiv2 = $("<td>");
-            tdDiv1.text(drink['strIngredient' + i]);
-            tdDiv2.text(drink['strMeasure' + i]);
-            trDiv.append(tdDiv1, tdDiv2);
-            $("#drinkIngredient").append(trDiv);
-            i++;
-        } else {
-            stop = true;
-        };
-    };
-
+function dispDrink(res) {
+  console.log(res);
+  var drink = res.drinks[0];
+  var i = 1;
+  var stop = false;
+  $("#drinkName").text(drink.strDrink);
+  $("#drinkCategory").text(drink.strCategory);
+  $("#drinkAlcoholic").text(drink.strAlcoholic);
+  $("#drinkGlass").text(drink.strGlass);
+  $("#drinkInstructions").text(drink.strInstructions);
+  $("#drinkImage").attr("src", drink.strDrinkThumb);
+  while (!stop) {
+    if (drink["strIngredient" + i]) {
+      var trDiv = $("<tr>");
+      var tdDiv1 = $("<td>");
+      var tdDiv2 = $("<td>");
+      tdDiv1.text(drink["strIngredient" + i]);
+      tdDiv2.text(drink["strMeasure" + i]);
+      trDiv.append(tdDiv1, tdDiv2);
+      $("#drinkIngredient").append(trDiv);
+      i++;
+    } else {
+      stop = true;
+    }
+  }
 }
